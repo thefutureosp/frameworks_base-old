65b5fcf2602c46f2474816c623e3674c914ababd
Orca Project <shamukillaz@orcaproject.co>
2013-08-19 22:46:44 -0400

    QS tiles background mod.[1/2] Option to set the color of QS background and the color when pressed.
    Change-Id: I8b3b5b1b7efd5ee79608ae7aaddf4d81ef770acd

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2874,6 +2874,14 @@ protected WindowManager.LayoutParams getRecentsLayoutParams(LayoutParams layoutP
             cr.registerContentObserver(
                     Settings.System.getUriFor(Settings.System.QS_DYNAMIC_WIFI),
                     false, this);
+                    
+            cr.registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.QUICK_TILES_BG_COLOR),
+                    false, this);
+
+            cr.registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.QUICK_TILES_BG_PRESSED_COLOR),
+                    false, this);                   
         }
     }
 }
