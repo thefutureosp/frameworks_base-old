36d39252c76fab08c184b9347fba4da2157a93ff
Orca Project <shamukillaz@orcaproject.co>
2013-08-19 22:35:56 -0400

    QuickSettings Camera Tile [2/2] Change-Id: I64d6b604cee167c376652408ebf4ed334c91462c

--- packages/SystemUI/src/com/android/systemui/quicksettings/QuickSettingsTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/QuickSettingsTile.java
@@ -71,10 +71,13 @@ public class QuickSettingsTile implements OnClickListener {
     }
 
     void updateQuickSettings(){
-        TextView tv = (TextView) mTile.findViewById(R.id.text);
-        ImageView iv = (ImageView) mTile.findViewById(R.id.image);
-        tv.setText(mLabel);
-        iv.setImageDrawable(mContext.getResources().getDrawable(mDrawable));
+        if (tv != null) {
+            tv.setCompoundDrawablesWithIntrinsicBounds(0, mDrawable, 0, 0);
+            tv.setText(mLabel);
+            tv.setTextSize(1, mTileTextSize);
+            if (mTileTextColor != -2) {
+                tv.setTextColor(mTileTextColor);
+            }
     }
 
     void startSettingsActivity(String action) {
@@ -104,8 +107,11 @@ public class QuickSettingsTile implements OnClickListener {
     }
 
     @Override
-    public final void onClick(View v) {
-        mOnClick.onClick(v);
+    public void onClick(View v) {
+        if (mOnClick != null) {
+            mOnClick.onClick(v);
+        }
+
         ContentResolver resolver = mContext.getContentResolver();
         boolean shouldCollapse = Settings.System.getInt(resolver, Settings.System.QS_COLLAPSE_PANEL, 0) == 1;
         if (shouldCollapse || this instanceof DesktopModeTile || this instanceof HybridTile) {
